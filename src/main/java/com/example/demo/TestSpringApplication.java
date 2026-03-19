8d6b3e82c59134a46f98e611da4ffc1dca8e2c11
@harshaldhanewar <harshaldhanewar@gmail.com>
2026-03-19 20:12:44 +0530

    change the file

--- src/main/java/com/example/demo/TestSpringApplication.java
+++ src/main/java/com/example/demo/TestSpringApplication.java
@@ -10,6 +10,7 @@ public class TestSpringApplication {
 
 		SpringApplication.run(TestSpringApplication.class, args);
 		System.out.println("SPRING BOOT APPLICATION IS STARTED");
+		System.out.println("Did the changes");
 	}
 
 }
